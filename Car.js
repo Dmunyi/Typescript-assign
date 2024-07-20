d3f0fa1423e41e1c5a87bd460cd16e805b07c1e9
Derrick Munyi <152088668+Dmunyi@users.noreply.github.com>
2024-07-20 10:44:29 +0300

    Add files via upload

---
+++ Car.js
@@ -0,0 +1,15 @@
+// Compiled JavaScript code
+class car {
+    constructor(make, model, year) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+  
+    start() {
+      console.log("Car engine started");
+    }
+  }
+  
+  const myCar = new Car("Toyota", "Corolla", 2022);
+  myCar.start();
